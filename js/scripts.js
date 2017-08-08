d621f511425d625689386be2d87a52bafcc40579
Donna Lambert <dlambertpdx@gmail.com>
2017-08-08 16:09:40 -0700

    correction to $

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(function() {
+  $(".js-clickable").click(function() {
+    $(".initially-hidden").fadeToggle();
+    $(".initially-showing").fadeToggle();
+  });
 
+  $(".op-clickable").click(function() {
+    $(".initially-hidden").fadeToggle();
+    $(".initially-showing").fadeToggle();
+  });
 });
